f6c1bb1de22695c597eb8a05636b2a9cc47df283
barryjj <barryjj@gmail.com>
2019-03-15 22:13:03 -0400

    Initial project commit.

---
+++ GameNewsTwitterFormatter/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.outputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.Location = new System.Drawing.Point(95, 12);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(693, 20);
+            this.urlTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Target URL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // outputRichTextBox
+            // 
+            this.outputRichTextBox.Location = new System.Drawing.Point(95, 56);
+            this.outputRichTextBox.Name = "outputRichTextBox";
+            this.outputRichTextBox.Size = new System.Drawing.Size(693, 285);
+            this.outputRichTextBox.TabIndex = 2;
+            this.outputRichTextBox.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tweet Content";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(713, 364);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Format";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 405);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.outputRichTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.urlTextBox);
+            this.Name = "Form1";
+            this.Text = "Game News Tweet Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox outputRichTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ GameNewsTwitterFormatter/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string URL = urlTextBox.Text;
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+
+            /* Looking for steampowered.com or youtube.com addresses.
+             * Checks only for youtu in the string provided because it's easier;
+             * Why check for both youtu.be and youtube when we can just check for youtu, right?
+             */
+            if (URL.Contains("steampowered") || URL.Contains("youtu"))
+            {
+                doc = web.Load(URL);
+                if (URL.Contains("steampowered"))
+                {
+                    /*********************
+                     * [GAME_NAME] <meta property="og:title" content="MONSTER HUNTER: WORLD on Steam">
+                     * [STORE_LINK] <meta property="og:url" content="https://store.steampowered.com/app/582010/MONSTER_HUNTER_WORLD/">
+                     * [COVER_IMAGE] <meta property="og:image" content="https://steamcdn-a.akamaihd.net/steam/apps/582010/header.jpg?t=1533890901">
+                       ******************/
+
+                    var nameNode = doc.DocumentNode.SelectSingleNode("//meta[@property='og:title']");
+                    var urlNode = doc.DocumentNode.SelectSingleNode("//meta[@property='og:url']");
+                    var coverImageNode = doc.DocumentNode.SelectSingleNode("//meta[@property='og:image']");
+
+                    // grab the attributes of the game lash requested
+                    string name = nameNode.Attributes["content"].Value;
+                    string url = urlNode.Attributes["content"].Value;
+                    string coverImage = coverImageNode.Attributes["content"].Value;
+
+                    // now we need to massage the text strings for name and url
+                    var trimIndex = name.IndexOf(" on Steam");
+                    name = name.Remove(trimIndex);
+                    // remove the last /
+                    url = url.Remove(url.Length - 1, 1);
+                    trimIndex = url.LastIndexOf("/");
+                    url = url.Remove(trimIndex);
+
+                    outputRichTextBox.Text = name + "\n" +
+                                             url + "\n" +
+                                             coverImage;
+                }
+                else if (URL.Contains("youtu"))
+                {
+                    /**********************
+                     * [VIDEO_NAME]
+                     * [VIDEO_LINK] {full link ... NOT youtu.be ... also without all the ref trash ... so for example: https://www.youtube.com/watch?v=riJ5JdF2lSY instead of https://www.youtube.com/watch?v=riJ5JdF2lSY&feature=em-uploademail}
+                     * ********************/
+                }
+            }
+            else
+            {
+                outputRichTextBox.Text = "Unrecognized URL: " + URL + ".\n" + "Domain is not supported at this time.";
+            }
+        }
+    }
+}
